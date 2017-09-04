5659d7ed6c09bba8b0635acc75f3617f60a6e240
Chris Buffington <christopher.k.buffington@boeing.com>
2017-09-04 19:26:38 -0400

    New. add some files

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,9 +4,23 @@ namespace ConsoleApp1
 {
     class Program
     {
+        protected Program()
+        {
+
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
         }
     }
 }
